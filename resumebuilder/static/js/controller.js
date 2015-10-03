89aeaf90e9f57a7fe45dfdb0d3a0c661cdf353e5
shubDhond <shubham.dhond@mail.utoronto.ca>
2015-10-03 00:47:03 -0400

    changed static files

--- resumebuilder/static/js/controller.js
+++ resumebuilder/static/js/controller.js
@@ -1,4 +1,7 @@
 /**
  * Created by sdhond on 2015-10-02.
  */
-var myApp = angular.module('myApp',[]);
+var myApp = angular.module('myApp',['textAngular']);
+
+myApp.controller('ctrl',['$scope','$http',function($scope,$http){
+}]);
